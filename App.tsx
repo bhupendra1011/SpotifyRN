eaacbefe8b4def803036966484fb61fa1d5e329d
Bhupendra <bhupendra1011@gmail.com>
2021-04-27 13:53:44 +0530

    aws  amplify setup

--- App.tsx
+++ App.tsx
@@ -7,6 +7,9 @@ import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
 
+
+
+
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
